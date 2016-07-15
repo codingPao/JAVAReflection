c4f610895d2a28e88047e455dd461916991c40db
codingpao <923451700@qq.com>
2016-07-15 15:24:46 +0800

    Person实体类

--- src/Reflect/Person.java
+++ src/Reflect/Person.java
@@ -4,4 +4,38 @@ package Reflect;
  * Created by long on 2016/7/12.
  */
 public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }

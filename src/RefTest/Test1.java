79ee9e09f127c358c996aea74cca800218c538ae
codingpao <923451700@qq.com>
2016-07-15 11:42:26 +0800

    通过一个对象获得完整的包名和类名

--- src/RefTest/Test1.java
+++ src/RefTest/Test1.java
@@ -1,7 +1,13 @@
 package RefTest;
 
+import Reflect.Demo;
+
 /**
- * Created by long on 2016/7/13.
+ * 通过一个对象获得完整的包名和类名
  */
 public class Test1 {
+    public static void main(String[] args){
+        Demo demo=new Demo();
+        System.out.println(demo.getClass().getName());
+    }
 }

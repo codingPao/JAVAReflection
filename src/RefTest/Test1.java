64800814ebec37d8fb66e71517458da6e90c90c9
codingpao <923451700@qq.com>
2016-07-15 13:46:34 +0800

    通过一个对象获得完整的报名和类名

--- src/RefTest/Test1.java
+++ src/RefTest/Test1.java
@@ -6,8 +6,12 @@ import Reflect.Demo;
  * 通过一个对象获得完整的包名和类名
  */
 public class Test1 {
-    public static void main(String[] args){
-        Demo demo=new Demo();
+    public static void main(String[] args) {
+        Demo demo = new Demo();
         System.out.println(demo.getClass().getName());
+        /**
+         * 运行结果:
+         *     Reflect.Demo
+         */
     }
 }
